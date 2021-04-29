919f9fbaa216978fbe3ffc5880a201dcbdc7b7cd
George Kramer <george.kramer@live.com>
2021-04-28 23:16:47 -0700

    Remove A2DP GDM workaround
    Arch Linux supplies a local default.pa for the gdm user.  It will use
    this instead of the global default.pa in /etc, which will try to load
    bluetooth modules.

--- chroot/bluetooth.sh
+++ chroot/bluetooth.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
 # Install Bluetooth components
 pacman -Sy --noconfirm bluez bluez-utils bluez-plugins pulseaudio-bluetooth gnome-bluetooth gnome-user-share
 systemctl enable bluetooth
@@ -9,8 +7,3 @@ systemctl enable bluetooth
 if ! grep -Pq '^load-module module-switch-on-connect$' /etc/pulse/default.pa; then
 	echo 'load-module module-switch-on-connect' >> /etc/pulse/default.pa
 fi
-
-# Prevent GDM from capturing the A2DP sink on startup
-mkdir -p ~gdm/.config/pulse
-cp -f "$SCRIPT_DIR/default.pa" ~gdm/.config/pulse
-chown -R gdm:gdm ~gdm/.config
