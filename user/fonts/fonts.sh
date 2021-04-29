6b72c3ae1c4aaec2b311b73bb9f796468a16a6fe
George Kramer <george.kramer@live.com>
2021-04-28 23:03:32 -0700

    Update font configuration
    Install Microsoft fonts for the web
    
    Use current fontconfig configuration locations

--- user/fonts/fonts.sh
+++ user/fonts/fonts.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 sudo pacman -Sy --noconfirm $(sudo pacman -Ssq adobe-source-.*-fonts)
-aurman -Sy ttf-ms-win10
+aurman -Sy ttf-ms-fonts
 
-sudo cp -f "$SCRIPT_DIR/99-generic-family.conf" /etc/fonts/conf.avail
-sudo ln -sf /etc/fonts/conf.avail/99-generic-family.conf /etc/fonts/conf.d
+sudo cp -f "$SCRIPT_DIR/99-generic-family.conf" /usr/share/fontconfig/conf.avail
+sudo ln -sf /usr/share/fontconfig/conf.avail/99-generic-family.conf /etc/fonts/conf.d
