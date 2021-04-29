2abf7feb4eca7610c69b5fabb1e4132d6ecfacd0
George Kramer <george.kramer@live.com>
2021-04-28 23:26:51 -0700

    Do not redundantly reinstall packages
    This was being done with packages from the gnome package group

--- user/packages/packages.sh
+++ user/packages/packages.sh
@@ -3,19 +3,10 @@
 # Internet
 sudo pacman -Sy --noconfirm firefox chrome-gnome-shell
 
-# Communications
-sudo pacman -Sy --noconfirm polari
-
-# Office
-aurman -Sy --noconfirm --noedit libreoffice-fresh
-
 # Development
-aurman -Sy --noconfirm --noedit dotnet-sdk hugo
+aurman -Sy --noconfirm --noedit dotnet-sdk hugo shellcheck
 
 ## EDIT
-# Text
-sudo pacman -Sy --noconfirm gedit
-
 # Pictures
 sudo pacman -Sy --noconfirm gimp hugin gimp-nufraw
 
@@ -23,31 +14,23 @@ sudo pacman -Sy --noconfirm gimp hugin gimp-nufraw
 sudo pacman -Sy --noconfirm handbrake
 
 ## VIEW AND PLAY
-# Viewers
-sudo pacman -Sy --noconfirm evince eog
-
-# Players
-sudo pacman -Sy --noconfirm vlc ffmpegthumbnailer
-
 # Frameworks
+# Switch to intel-media-driver from libva-intel-driver after getting a current Intel CPU
 sudo pacman -Sy --noconfirm gstreamer gstreamer-vaapi libva-intel-driver gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav
 
 ## SYSTEM TOOLS
-# File Management
-sudo pacman -Sy --noconfirm nautilus file-roller p7zip baobab
-
 # Disk Management
-sudo pacman -Sy --noconfirm gnome-disk-utility dosfstools e2fsprogs exfat-utils f2fs-tools hdparm
+sudo pacman -Sy --noconfirm dosfstools e2fsprogs exfat-utils f2fs-tools hdparm
 
 # System Administration
-aurman -Sy --noconfirm --noedit gnome-terminal gnome-system-monitor hardinfo-git dconf-editor gnome-logs man-db
+aurman -Sy --noconfirm --noedit hardinfo-git dconf-editor man-db
 
 # Accessories
-sudo pacman -Sy --noconfirm gnome-font-viewer gnome-characters gnome-screenshot gnome-calculator gnome-tweaks cheese simple-scan gnome-clocks yelp
+sudo pacman -Sy --noconfirm gnome-tweaks
 
 ## CUSTOM PACKAGES
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-PACKAGES=(vim git visual-studio-code tresorit spotify)
+PACKAGES=(vim git visual-studio-code tresorit spotify yubi unifi)
 for PACKAGE in "${PACKAGES[@]}"; do
 	PACKAGE_PATH="$SCRIPT_DIR/$PACKAGE.sh"
 	if [ ! -e "$PACKAGE_PATH" ]; then
@@ -55,6 +38,3 @@ for PACKAGE in "${PACKAGES[@]}"; do
 	fi
 	"$PACKAGE_PATH"
 done
-
-## REMOVE EXTRA PACKAGES
-sudo pacman -Rns --noconfirm epiphany gnome-software gnome-boxes gnome-maps gnome-music gnome-photos gnome-weather gnome-todo totem gnome-documents gnome-calendar gnome-books gnome-contacts gnome-dictionary
