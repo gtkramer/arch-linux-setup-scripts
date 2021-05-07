4400382f66faa6fdaa1c4d242e5019a7dd7c0280
George Kramer <george.kramer@live.com>
2021-05-06 23:03:55 -0700

    Fix printing script
    lsusb is required when running hp-setup to avoid an error.
    
    Want to socket enable cups, since printing is not common.  This is
    enough to get printing to work in GNOME Settings.

--- user/devices/printing.sh
+++ user/devices/printing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo pacman -Sy --noconfirm cups hplip python-pyqt5
-sudo systemctl enable --now org.cups.cupsd
+sudo pacman -Syu --noconfirm cups hplip usbutils python-pyqt5
+sudo systemctl enable --now cups.socket
 hp-setup
