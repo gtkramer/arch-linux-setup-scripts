9be5016496c2298f100bf6123ed9464323e994a4
George Kramer <george.kramer@live.com>
2021-06-25 19:52:00 -0700

    Use more Microsoft fonts
    Growing up with Windows, I am used to Microsoft fonts.  Make these the
    default fonts used for Linux.
    
    Override Cantarell with sans-serif to change out all instances where
    this font is used in GNOME Shell.

--- user/fonts/fonts.sh
+++ user/fonts/fonts.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-sudo pacman -Sy --noconfirm $(sudo pacman -Ssq adobe-source-.*-fonts)
-aurman -Syu ttf-ms-fonts
+sudo pacman -Sy --noconfirm $(sudo pacman -Ssq adobe-source-.*-fonts) otf-cascadia-code
+aurman -Syu --noconfirm --noedit ttf-ms-fonts
+sudo find "$HOME/Documents/Fonts/TTF" -iname '*.ttf' -exec cp {} /usr/share/fonts/TTF/ \;
 
-sudo cp -f "$SCRIPT_DIR/99-generic-family.conf" /usr/share/fontconfig/conf.avail
+sudo cp -f "$SCRIPT_DIR/99-generic-family.conf" "$SCRIPT_DIR/98-gnome.conf" /usr/share/fontconfig/conf.avail
 sudo ln -sf /usr/share/fontconfig/conf.avail/99-generic-family.conf /etc/fonts/conf.d
+sudo ln -sf /usr/share/fontconfig/conf.avail/98-gnome.conf /etc/fonts/conf.d
