bfc299bdcd28ef504061a022c34c9de71d7b2a98
George Kramer <george.kramer@live.com>
2022-07-09 21:44:52 -0700

    Update parameters
    Fix hostname, fully qualify variables, and have all pacman commands
    perform and update to follow aurman's lead

--- parameters.sh
+++ parameters.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export USERNAME=george
 export DISPLAY_NAME='George Kramer'
-export GIT_NAME="$DISPLAY_NAME"
+export GIT_NAME="${DISPLAY_NAME}"
 export GIT_EMAIL=george.kramer@live.com
 export GIT_EDITOR=vim
 export TIMEZONE=America/Los_Angeles
-export HOSTNAME=thinkpad-t440s
+export HOSTNAME=tufh670pro
 export LOCALE=en_US.UTF-8
 export KEYMAP=us
 export COUNTRY_MIRROR='United States'
-export PACMAN='pacman -Sy --noconfirm'
+export PACMAN='pacman -Syu --noconfirm'
 export AURMAN='aurman -Syu --noconfirm --noedit'
