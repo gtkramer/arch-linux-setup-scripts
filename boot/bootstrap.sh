f25b36572695beb3967f889bbc90e9f2db145d2d
George Kramer <george.kramer@live.com>
2022-07-09 21:30:20 -0700

    Fully qualify variables in boot scripts

--- boot/bootstrap.sh
+++ boot/bootstrap.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-source "$SCRIPT_DIR/../parameters.sh"
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+source "${SCRIPT_DIR}/../parameters.sh"
 
 display_help() {
 	local script_name
-	script_name="$(basename "$0")"
-	echo "Usage: $script_name -b|--block <block device>"
+	script_name="$(basename "${0}")"
+	echo "Usage: ${script_name} -b|--block <block device>"
 }
 
-PARAMS="$(getopt -o b:h -l block:,help --name "$0" -- "$@")"
-eval set -- "$PARAMS"
+PARAMS="$(getopt -o b:h -l block:,help --name "${0}" -- "${@}")"
+eval set -- "${PARAMS}"
 
 while true; do
-    case "$1" in
+    case "${1}" in
         -b|--block)
-            BLOCK_DEV="$2"
+            BLOCK_DEV="${2}"
             shift 2
             ;;
         -h|--help)
@@ -32,7 +32,7 @@ while true; do
     esac
 done
 
-if [ -z "$BLOCK_DEV" ]; then
+if [ -z "${BLOCK_DEV}" ]; then
 	echo 'Parameter -b|--block is required' >&2
 	exit 1
 fi
@@ -40,9 +40,9 @@ fi
 # Configure boot
 pacman -Sy --noconfirm efibootmgr
 efibootmgr | sed -nr 's/^Boot([[:digit:]]+).*Linux$/\1/p' | while read -r BOOT_NUM; do
-	efibootmgr -b "$BOOT_NUM" -B
+	efibootmgr -b "${BOOT_NUM}" -B
 done
-efibootmgr -c -d "$BLOCK_DEV" -p 1 -L 'Arch Linux' -l /vmlinuz-linux -u 'cryptdevice=PARTLABEL=root:root root=/dev/mapper/root rw initrd=/initramfs-linux.img nvidia-drm.modeset=1 quiet'
+efibootmgr -c -d "${BLOCK_DEV}" -p 1 -L 'Arch Linux' -l /vmlinuz-linux -u 'cryptdevice=PARTLABEL=root:root root=/dev/mapper/root rw initrd=/initramfs-linux.img nvidia-drm.modeset=1 quiet'
 
 # Configure hooks
 sed -i '/^HOOKS=/d' /etc/mkinitcpio.conf
@@ -55,9 +55,9 @@ passwd root
 
 mkdir -p /etc/sudoers.d
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
-useradd -m -G wheel -c "$DISPLAY_NAME" "$USERNAME"
-echo "Set password for $USERNAME"
-passwd "$USERNAME"
+useradd -m -G wheel -c "${DISPLAY_NAME}" "${USERNAME}"
+echo "Set password for ${USERNAME}"
+passwd "${USERNAME}"
 
 # Enable network to come up automatically
 pacman -Sy --noconfirm networkmanager
--- boot/install.sh
+++ boot/install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-source "$SCRIPT_DIR/../parameters.sh"
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+source "${SCRIPT_DIR}/../parameters.sh"
 
 display_help() {
 	local script_name
-	script_name="$(basename "$0")"
-	echo "Usage: $script_name -b|--block <block device>"
+	script_name="$(basename "${0}")"
+	echo "Usage: ${script_name} -b|--block <block device>"
 }
 
-PARAMS="$(getopt -o b:h -l block:,help --name "$0" -- "$@")"
-eval set -- "$PARAMS"
+PARAMS="$(getopt -o b:h -l block:,help --name "${0}" -- "${@}")"
+eval set -- "${PARAMS}"
 
 while true; do
-    case "$1" in
+    case "${1}" in
         -b|--block)
-            BLOCK_DEV="$2"
+            BLOCK_DEV="${2}"
             shift 2
             ;;
         -h|--help)
@@ -32,7 +32,7 @@ while true; do
     esac
 done
 
-if [ -z "$BLOCK_DEV" ]; then
+if [ -z "${BLOCK_DEV}" ]; then
 	echo 'Parameter -b|--block is required' >&2
 	exit 1
 fi
@@ -41,10 +41,10 @@ fi
 timedatectl set-ntp true
 
 # Create partitions
-sgdisk -Z "$BLOCK_DEV"
-sgdisk -n 1:1M:+128M -t 1:ef00 -c 1:boot "$BLOCK_DEV"
-sgdisk -n 2:0:0 -t 2:8304 -c 2:root "$BLOCK_DEV"
-if ! sgdisk -v "$BLOCK_DEV"; then
+sgdisk -Z "${BLOCK_DEV}"
+sgdisk -n 1:1M:+128M -t 1:ef00 -c 1:boot "${BLOCK_DEV}"
+sgdisk -n 2:0:0 -t 2:8304 -c 2:root "${BLOCK_DEV}"
+if ! sgdisk -v "${BLOCK_DEV}"; then
 	echo "Drive partitions failed verification for ${BLOCK_DEV}" >&2
 	exit 1
 fi
@@ -55,28 +55,28 @@ DEV_ROOT="${BLOCK_DEV}p2"
 # Create file systems
 mkfs.fat -F32 "${DEV_BOOT}"
 
-cryptsetup -y -v luksFormat "$DEV_ROOT"
-cryptsetup --allow-discards --perf-no_read_workqueue --perf-no_write_workqueue --persistent open "$DEV_ROOT" root
+cryptsetup -y -v luksFormat "${DEV_ROOT}"
+cryptsetup --allow-discards --perf-no_read_workqueue --perf-no_write_workqueue --persistent open "${DEV_ROOT}" root
 MAP_ROOT=/dev/mapper/root
-mkfs.ext4 -F "$MAP_ROOT"
+mkfs.ext4 -F "${MAP_ROOT}"
 
 # Mount file systems
 mkdir -p /mnt
-mount "$MAP_ROOT" /mnt
+mount "${MAP_ROOT}" /mnt
 mkdir -p /mnt/boot
 mount "${DEV_BOOT}" /mnt/boot
 
 # Configure mirrors
 pacman -Sy --noconfirm reflector
-reflector --country "$COUNTRY_MIRROR" --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+reflector --country "${COUNTRY_MIRROR}" --sort rate --protocol https --save /etc/pacman.d/mirrorlist
 
 # Install system
 pacstrap /mnt base base-devel linux linux-firmware
 
 # Configure file systems
 FSTAB_FILE=/mnt/etc/fstab
-genfstab -L /mnt >> "$FSTAB_FILE"
+genfstab -L /mnt >> "${FSTAB_FILE}"
 
-sed -r -i "s_^${MAP_ROOT}(\\s+\\S+){3}_&,discard_" "$FSTAB_FILE"
+sed -r -i "s_^${MAP_ROOT}(\\s+\\S+){3}_&,discard_" "${FSTAB_FILE}"
 
-sed -i "s_^${DEV_BOOT}_PARTLABEL=boot_" "$FSTAB_FILE"
+sed -i "s_^${DEV_BOOT}_PARTLABEL=boot_" "${FSTAB_FILE}"
