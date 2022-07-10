280cfb10507c49beed91f5d61a6752ef57c7dccb
George Kramer <george.kramer@live.com>
2022-07-09 22:06:39 -0700

    Remove ramdisk.sh script
    New modules are removed periodically, not worth the effort to keep the
    mkinitcpio messages clean

--- user/ramdisk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
-source "${SCRIPT_DIR}/../../parameters.sh"
-
-${AURMAN_INSTALL} wd719x-firmware aic94xx-firmware upd72020x-fw
-mkinitcpio -p linux
