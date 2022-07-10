53d7c2665aef754399cddd3b2dad9ee059caf38b
George Kramer <george.kramer@live.com>
2022-07-09 21:33:13 -0700

    Remove PulseAudio configurations from bluetooth scripts

--- chroot/bluetooth.sh
+++ chroot/bluetooth.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Install Bluetooth components
-pacman -Sy --noconfirm bluez bluez-utils bluez-plugins pulseaudio-bluetooth gnome-bluetooth gnome-user-share
+pacman -Sy --noconfirm bluez bluez-utils bluez-plugins gnome-bluetooth gnome-user-share
 systemctl enable bluetooth
-
-# Automatically switch to Bluetooth headphones on connect
-if ! grep -Pq '^load-module module-switch-on-connect$' /etc/pulse/default.pa; then
-	echo 'load-module module-switch-on-connect' >> /etc/pulse/default.pa
-fi
