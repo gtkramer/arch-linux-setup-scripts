b90091974114001db9f3fa5ded37e1179a2fc73b
George Kramer <george.kramer@live.com>
2022-07-11 14:15:01 -0700

    Isolate NVIDIA configurations
    The GBM backend will automatically be used provided the following
    conditions are met without the need for setting environment variables:
    
    https://download.nvidia.com/XFree86/Linux-x86_64/510.39.01/README/gbm.html
    
    Wayland will be enabled by GDB provided the conditions for disabling
    wayland are NOT met:
    
    /usr/lib/udev/rules.d/61-gdm.rules
    
    Groups the XDG desktop portal packages with PipeWire because this is
    what necessitates their need due to Wayland security protocols with
    WebRTC.

--- chroot/desktop.sh
+++ chroot/desktop.sh
@@ -5,10 +5,17 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
 # Video capabilities
-${PACMAN_INSTALL} nvidia nvidia-utils xdg-desktop-portal xdg-desktop-portal-gnome
+${PACMAN_INSTALL} nvidia nvidia-utils
+systemctl enable nvidia-{hibernate,suspend,resume}
+cat >> /etc/modprobe.d/nvidia.conf <<EOF
+options nvidia NVreg_PreserveVideoMemoryAllocations=1
+options nvidia-drm modeset=1
+EOF
+mkinitcpio -p linux
 
 # Audio capabilities
 ${PACMAN_INSTALL} pipewire pipewire-alsa pipewire-pulse pipewire-jack
+${PACMAN_INSTALL} xdg-desktop-portal xdg-desktop-portal-gnome
 
 # Desktop environment
 ${PACMAN_INSTALL} gnome
