cd899c04b8b11e70087d4e8afa8dc8f9ccc6721e
George Kramer <george.kramer@live.com>
2022-07-11 15:15:17 -0700

    Improve quality of maintenance script execution
    Given `set -e` is being used, need to determine if there are orphaned
    packages to remove before attempting a removal.
    
    Automatically answer prompts to pacman to clear package cache and remove
    unused repositories.

--- maintenance.sh
+++ maintenance.sh
@@ -5,5 +5,8 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/parameters.sh"
 
 ${AURMAN_INSTALL} --do_everything
-sudo pacman -Rns $(sudo pacman -Qdtq)
-sudo pacman -Sc
+mapfile -t ORPHANED_PACKAGES < <(sudo pacman -Qdtq)
+if [[ "${#ORPHANED_PACKAGES[@]}" -ne 0 ]]; then
+    sudo pacman -Rns "${ORPHANED_PACKAGES[@]}"
+fi
+printf "Y\nY\n" | sudo pacman -Sc
