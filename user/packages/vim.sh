bcfe6073b6ff60eea81ed2096e28f61044a31401
George Kramer <george.kramer@live.com>
2022-07-15 20:14:55 -0700

    Better set VISUAL
    This is a user customization, not a global customization.  Different
    users may want different VISUAL editors for their terminal.

--- user/packages/vim.sh
+++ user/packages/vim.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../../parameters.sh"
 
 sudo ${PACMAN_INSTALL} vim
+
 if ! grep -Pq '^set spell$' /etc/vimrc; then
 	echo 'set spell' | sudo tee -a /etc/vimrc > /dev/null
 fi
-sed -i '/^export VISUAL/d' "${HOME}/.bashrc"
-sudo sed -i '/^export VISUAL/d' /etc/bash.bashrc
-echo "export VISUAL=vim" | sudo tee -a /etc/bash.bashrc > /dev/null
+
+if ! grep -Pq ' VISUAL=' "${HOME}/.bash_profile"; then
+	echo "export VISUAL=vim" >> "${HOME}/.bash_profile"
+fi
