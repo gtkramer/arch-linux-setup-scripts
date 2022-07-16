37b76eb3ff8a8e0397e522087f4d3b20ae4e6d5f
George Kramer <george.kramer@live.com>
2022-07-15 20:16:53 -0700

    Better set PS1
    PS1 is ultimately a user decision, but want to provide a better default.
    Do this by modifying the bashrc skeleton so that it gets created for
    each user on first login.
    
    Add the installation of gnome-terminal to this script because it makes
    sense with the extra vte customization, which is only needed for
    GTK-based terminal emulators.

--- chroot/terminal.sh
+++ chroot/terminal.sh
@@ -4,9 +4,10 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
-sed -i '/^PS1/d' "${HOME}/.bashrc"
-sed -i '/^PS1/d' /etc/bash.bashrc
-echo 'PS1="\[\033[38;5;33m\][\u@\h:\w] (\$?)\[$(tput sgr0)\]\n\[\033[38;5;172m\]\\$\[$(tput sgr0)\] "' | tee -a /etc/bash.bashrc > /dev/null
+sudo ${PACMAN_INSTALL} gnome-terminal
+
+sed -i '/^PS1/d' /etc/skel/.bashrc
+echo 'PS1="\[\033[38;5;33m\][\u@\h:\w] (\$?)\[$(tput sgr0)\]\n\[\033[38;5;172m\]\\$\[$(tput sgr0)\] "' | tee -a /etc/skel/.bashrc > /dev/null
 
 if ! grep -Pq '^source /etc/profile.d/vte.sh$' /etc/bash.bashrc; then
 	echo 'source /etc/profile.d/vte.sh' | tee -a /etc/bash.bashrc > /dev/null
