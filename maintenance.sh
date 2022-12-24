e4dfbcbeb5da4b844114195d91db8828eb09d301
George Kramer <george.kramer@live.com>
2022-12-24 14:04:07 -0800

    Really make maintenance non-interactive
    Always confirm removal of orphaned packages

--- maintenance.sh
+++ maintenance.sh
@@ -7,6 +7,6 @@ source "${SCRIPT_DIR}/parameters.sh"
 ${AURMAN_INSTALL} --do_everything
 mapfile -t ORPHANED_PACKAGES < <(sudo pacman -Qdtq)
 if [[ "${#ORPHANED_PACKAGES[@]}" -ne 0 ]]; then
-    sudo pacman -Rns "${ORPHANED_PACKAGES[@]}"
+    sudo pacman -Rns --noconfirm "${ORPHANED_PACKAGES[@]}"
 fi
 sudo pacman -Sc --noconfirm
