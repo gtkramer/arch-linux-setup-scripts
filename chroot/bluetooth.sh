c1dff83a40021aec99d548a6a5d850d527a9557e
George Kramer <george.kramer@live.com>
2022-12-24 12:30:13 -0800

    Switch to current version of GNOME Bluetooth

--- chroot/bluetooth.sh
+++ chroot/bluetooth.sh
@@ -4,5 +4,5 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
-${PACMAN_INSTALL} bluez bluez-utils bluez-plugins gnome-bluetooth gnome-user-share
+${PACMAN_INSTALL} bluez bluez-utils bluez-plugins gnome-bluetooth-3.0
 systemctl enable bluetooth
