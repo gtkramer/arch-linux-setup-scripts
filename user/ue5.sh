8e5109087d12aa858cf2f4435bcca625cac7904a
George Kramer <george.kramer@live.com>
2023-03-07 23:41:07 -0800

    Add Unreal Engine 5 setup script
    It's a pipe dream to do work with this.  Needing to prioritize a clean
    home office setup that can seamlessly switch KVM + other devices between
    home and work computers to minimize crazy wire hell is more important at
    the moment.
    
    Though the monitors no longer have VRR and are now susceptible to
    tearing again, development is still possible and it might be picked up
    one day.

---
+++ user/ue5.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+source "${SCRIPT_DIR}/../parameters.sh"
+
+BUILD_DIR="${HOME}/Builds/Epic/UnrealEngine"
+if [[ ! -e "${BUILD_DIR}/.git" ]]; then
+    rm -rf "${BUILD_DIR}"
+    mkdir -p "${BUILD_DIR}"
+    git clone git@github.com:EpicGames/UnrealEngine.git -b release "${BUILD_DIR}"
+else
+    pushd "${BUILD_DIR}"
+    git reset --hard origin/release
+    git clean -dxf
+    git pull
+    git clean -dxf
+    popd
+fi
+
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+sudo ln -sf /etc/ssl /usr/lib/ssl
+
+cd "${BUILD_DIR}"
+./Setup.sh
+./GenerateProjectFiles.sh
+make
