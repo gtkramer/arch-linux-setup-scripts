5414f1ae0bf35e3666db414c7313e5490707dec2
George Kramer <george.kramer@live.com>
2023-03-07 23:27:51 -0800

    No longer install Spotify desktop app
    The display scaling on this is crummy.  It's better to instead use the
    Spotify web app with Firefox, which works great with HiDPI displays.

--- user/packages/spotify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
-source "${SCRIPT_DIR}/../../parameters.sh"
-
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg --import -
-${AURMAN_INSTALL} spotify
