9c9765abacaa93cc18fe60083204620897c98c3a
George Kramer <george.kramer@live.com>
2023-07-09 13:02:30 -0700

    virtualization: Add support for Windows 11
    Need to simulate a TPM in the guest OS, and have UEFI firmware around

--- user/virtualization.sh
+++ user/virtualization.sh
@@ -4,5 +4,6 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
-sudo pacman -S gnome-boxes iptables-nft dnsmasq dmidecode
+sudo pacman -S gnome-boxes iptables-nft dnsmasq dmidecode edk2-ovmf swtpm
 sudo systemctl enable --now libvirtd.service
+swtpm_setup --create-config-files skip-if-exist
