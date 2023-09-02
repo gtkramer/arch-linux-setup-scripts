b51831436167cee9504b1c87be9261488f7ae88b
George Kramer <george.kramer@live.com>
2023-09-02 02:29:11 -0700

    Use the reflector service rather than a pacman hook
    The mirror list is rarely updated.  It's better to have a service that
    periodically speed tests mirrors as needed.  This is the current
    recommendation.

--- user/package-manager.sh
+++ user/package-manager.sh
@@ -8,27 +8,21 @@ sudo ${PACMAN_INSTALL} git
 TEMP_DIR="$(mktemp -d)"
 git clone https://aur.archlinux.org/aurman.git "${TEMP_DIR}"
 pushd "${TEMP_DIR}"
-gpg --recv-keys 465022E743D71E39
+gpg --recv-keys 910B8C499BED531B
 makepkg -sri
 popd
 rm -rf "${TEMP_DIR}"
 
 # Configure mirrors
 sudo ${PACMAN_INSTALL} reflector
-sudo reflector --country "${COUNTRY_MIRROR}" --sort rate --protocol https --save /etc/pacman.d/mirrorlist
-
-sudo mkdir -p /etc/pacman.d/hooks
-sudo tee /etc/pacman.d/hooks/mirrorupgrade.hook > /dev/null <<- EOF
-[Trigger]
-Operation = Upgrade
-Type = Package
-Target = pacman-mirrorlist
-
-[Action]
-Description = Updating pacman-mirrorlist with reflector...
-When = PostTransaction
-Depends = reflector
-Exec = /usr/bin/reflector --country "${COUNTRY_MIRROR}" --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+sudo mkdir -p /etc/xdg/reflector
+sudo tee /etc/xdg/reflector/reflector.conf <<- EOF
+--save /etc/pacman.d/mirrorlist
+--country "${COUNTRY_MIRROR}"
+--protocol https
+--sort rate
 EOF
+sudo systemctl start reflector
+sudo systemctl enable reflector.timer
 
 echo "MAKEFLAGS=\"-j$(nproc)\"" | sudo tee -a /etc/makepkg.conf > /dev/null
