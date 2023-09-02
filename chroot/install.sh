bbe20310071ce084a9ec9e581b2be411a8758ce9
George Kramer <george.kramer@live.com>
2023-09-02 01:44:50 -0700

    No longer modify vm.swappiness
    Believe that default values are set for a reason
    
    Also, decreasing swappiness prefers to keep programs in memory, which
    when full, makes new programs take a performance hit when unused
    programs should be swapped out of RAM and to the disk to make room in
    RAM for the new and active programs.
    
    This also doesn't really matter anymore since there is no swap.  Setting
    it up is complicated on a LUKS-encrypted partition with a swap file, and
    having a dedicated and encrypted partition for swap is even more complex
    to set up.  Also, swap is just not needed in this case due to the large
    amount of RAM in the system.

--- chroot/install.sh
+++ chroot/install.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
-CONFIGS=(desktop system terminal firewall kernel bluetooth)
+CONFIGS=(desktop system terminal firewall bluetooth)
 for CONFIG in "${CONFIGS[@]}"; do
 	CONFIG_PATH="${SCRIPT_DIR}/${CONFIG}.sh"
 	if [ ! -e "${CONFIG_PATH}" ]; then
--- chroot/kernel/kernel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
-source "${SCRIPT_DIR}/../../parameters.sh"
-
-cp -f "${SCRIPT_DIR}/99-kernel.conf" /etc/sysctl.d
