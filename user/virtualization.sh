b50174a3bc97c52b20ab25ae5c4bf9758afd6a1d
George Kramer <george.kramer@live.com>
2023-09-03 22:18:19 -0700

    Remove redundant user add to group wheel

--- user/virtualization.sh
+++ user/virtualization.sh
@@ -5,6 +5,5 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
 sudo pacman -S virt-manager qemu-desktop dnsmasq swtpm
-sudo usermod -a -G wheel "${USER}"
 sudo systemctl enable --now libvirtd
 swtpm_setup --create-config-files skip-if-exist
