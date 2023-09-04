1641c0538347d860cc48e3608ad420c3ac4e6d65
George Kramer <george.kramer@live.com>
2023-09-03 22:11:05 -0700

    Adjust terminal settings per user
    Modifying the user profile skeleton is presumptuous.  Let users do what
    they want.
    
    This didn't work anyway in the current workflow, since the .bashrc file
    is created at the time of user account creation and it's only after that
    this file was modified to include Powerline settings.

--- user/terminal.sh
+++ user/terminal.sh
@@ -4,10 +4,10 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
-${PACMAN_INSTALL} powerline powerline-fonts
+${PACMAN_INSTALL} powerline powerline-fonts vte-common
 
-if ! grep -q powerline-daemon /etc/skel/.bashrc; then
-	cat >> /etc/skel/.bashrc << EOF
+if ! grep -q powerline-daemon "${HOME}/.bashrc"; then
+	cat >> "${HOME}/.bashrc" << EOF
 powerline-daemon -q
 POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
@@ -15,6 +15,6 @@ POWERLINE_BASH_SELECT=1
 EOF
 fi
 
-if ! grep -q vte.sh /etc/skel/.bashrc; then
-	echo '. /etc/profile.d/vte.sh' >> /etc/skel/.bashrc
+if ! grep -q vte.sh "${HOME}/.bashrc"; then
+	echo '. /etc/profile.d/vte.sh' >> "${HOME}/.bashrc"
 fi
