a2a3431b5b87c699969b726e06cc9d7f4539e342
George Kramer <george.kramer@live.com>
2023-09-04 18:52:48 -0700

    Install virtualization software non-interactively

--- user/virtualization.sh
+++ user/virtualization.sh
@@ -4,6 +4,6 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 source "${SCRIPT_DIR}/../parameters.sh"
 
-sudo pacman -S virt-manager qemu-desktop dnsmasq swtpm
+sudo ${PACMAN_INSTALL} virt-manager qemu-desktop dnsmasq swtpm
 sudo systemctl enable --now libvirtd
 swtpm_setup --create-config-files skip-if-exist
