5c3a2433c1298b0dcce8c4c866c805c0dfec6baa
George Kramer <george.kramer@live.com>
2023-09-08 00:54:31 -0700

    Use VirtualBox over virt-manager
    VirtualBox has a Qt-based GUI and is much more user-friendly than virt-manager

--- user/virtualization.sh
+++ user/virtualization.sh
@@ -4,6 +4,4 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../parameters.sh"
 
-sudo ${PACMAN_INSTALL} virt-manager qemu-desktop dnsmasq swtpm
-sudo systemctl enable --now libvirtd
-swtpm_setup --create-config-files skip-if-exist
+sudo ${PACMAN_INSTALL} virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
