5d669dc273eb1775bb71f1a510f85dae00afc601
George Kramer <george.kramer@live.com>
2023-09-08 01:54:29 -0700

    Add network support for hplip setup
    Want to add a network-attached printer.  Install the optional packages that are
    network-related.

--- user/devices/printing.sh
+++ user/devices/printing.sh
@@ -4,6 +4,6 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../../parameters.sh"
 
-sudo ${PACMAN_INSTALL} print-manager system-config-printer cups hplip usbutils python-pyqt5
+sudo ${PACMAN_INSTALL} print-manager system-config-printer cups hplip usbutils python-pyqt5 wget rpcbind
 sudo systemctl enable --now cups.socket
 hp-setup
