3cce6d4fa21e530d1b40f350a23722c606d782ce
George Kramer <george.kramer@live.com>
2023-09-09 21:27:48 -0700

    Add user to vboxusers group to access USB devices

--- user/virtualization.sh
+++ user/virtualization.sh
@@ -5,3 +5,4 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../parameters.sh"
 
 sudo ${PACMAN_INSTALL} virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
+sudo usermod -a -G vboxusers "${USER}"
