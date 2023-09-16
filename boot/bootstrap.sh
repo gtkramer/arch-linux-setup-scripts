231f0f75abcef9a32b389ef86329e386028d2bf7
George Kramer <george.kramer@live.com>
2023-09-16 15:45:40 -0700

    Follow the default HOOKS more closely for initramfs

--- boot/bootstrap.sh
+++ boot/bootstrap.sh
@@ -47,7 +47,7 @@ efibootmgr -c -d "${BLOCK_DEV}" -p 1 -L 'Arch Linux' -l /vmlinuz-linux -u 'crypt
 
 # Configure hooks
 sed -i '/^HOOKS=/d' /etc/mkinitcpio.conf
-echo 'HOOKS=(base udev keyboard keymap consolefont autodetect modconf block encrypt lvm2 resume filesystems fsck)' >> /etc/mkinitcpio.conf
+echo 'HOOKS=(base udev autodetect modconf kms keyboard keymap consolefont block encrypt lvm2 resume filesystems fsck)' >> /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
 # Set up passwordless authentication based on group membership
