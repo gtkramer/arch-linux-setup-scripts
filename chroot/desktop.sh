bfbd8bde68be7d7fd65400806a0bd3f9af7ecfda
George Kramer <george.kramer@live.com>
2023-09-16 14:46:41 -0700

    Switch to Intel graphics
    Intel graphics have always worked best on Linux.  Provide extra
    utilities to verify functionality OOB, given we are working with Intel's
    first-generation discrete graphics cards.

--- chroot/desktop.sh
+++ chroot/desktop.sh
@@ -5,12 +5,7 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../parameters.sh"
 
 # Video capabilities
-${PACMAN_INSTALL} xorg-server nvidia nvidia-utils
-systemctl enable nvidia-{hibernate,suspend,resume}
-cat >> /etc/modprobe.d/nvidia.conf <<EOF
-options nvidia NVreg_PreserveVideoMemoryAllocations=1
-options nvidia-drm modeset=1
-EOF
+${PACMAN_INSTALL} xorg-server xorg-xwininfo mesa mesa-utils vulkan-intel vulkan-tools intel-media-driver libva-utils
 
 # Audio capabilities
 ${PACMAN_INSTALL} pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber
