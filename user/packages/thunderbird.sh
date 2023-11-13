f54e6faf67d94f63c7675e55f9466c3440f4c9ec
George Kramer <george.kramer@live.com>
2023-11-12 18:07:59 -0800

    General script cleanup for Mozilla Wayland

--- user/packages/thunderbird.sh
+++ user/packages/thunderbird.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../../parameters.sh"
 
 sudo ${PACMAN_INSTALL} thunderbird xdg-desktop-portal-gnome
-if ! grep -q "MOZ_ENABLE_WAYLAND" ~/.bash_profile; then
-    cat >> ~/.bash_profile <<EOL
-if [ "\$XDG_SESSION_TYPE" = "wayland" ]; then
+if ! grep -q "MOZ_ENABLE_WAYLAND" "${HOME}/.bash_profile"; then
+    cat >> "${HOME}/.bash_profile" <<EOL
+if [[ "\$XDG_SESSION_TYPE" == "wayland" ]]; then
     export MOZ_ENABLE_WAYLAND=1
 fi
 EOL
