9a6ee668801a081cf07fecbf2267788c2eb9f6fc
George Kramer <12485591+gtkramer@users.noreply.github.com>
2024-01-06 20:56:07 -0800

    Turn on night light for GDM

--- user/gdm.sh
+++ user/gdm.sh
@@ -9,3 +9,12 @@ sudo chown gdm:gdm ~gdm/.config/
 
 sudo cp -f "${HOME}/.config/monitors.xml" ~gdm/.config/
 sudo chown gdm:gdm ~gdm/.config/monitors.xml
+
+run_as_gdm_user() {
+    sudo -u gdm dbus-launch --exit-with-session "${@}"
+}
+
+run_as_gdm_user gsettings set org.gnome.system.location enabled true
+run_as_gdm_user gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+run_as_gdm_user gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic true
+run_as_gdm_user gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 2700
