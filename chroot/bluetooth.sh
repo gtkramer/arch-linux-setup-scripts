52ec404b7c5004c4782200c014704fea613047cf
George Kramer <12485591+gtkramer@users.noreply.github.com>
2024-03-31 12:17:48 -0700

    Update bluetooth package list
    bluez-plugins has been merged into bluez-utils

--- chroot/bluetooth.sh
+++ chroot/bluetooth.sh
@@ -4,5 +4,5 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../parameters.sh"
 
-${PACMAN_INSTALL} bluez bluez-utils bluez-plugins
+${PACMAN_INSTALL} bluez bluez-utils
 systemctl enable bluetooth
