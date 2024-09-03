b3d7c237e86ac00498fc9e996d52df4db46602e8
George Kramer <12485591+gtkramer@users.noreply.github.com>
2024-09-03 00:47:25 -0700

    Revert "Switch back to NVIDIA graphics"
    This reverts commit e2c4a71711ec44f30f4808b8bbcb34adbe1331a7.
    
    Video encoding with the NVIDIA RTX 3070 is 5.79% faster than the Intel ARC
    A770.  The 3070 also performs better with rendering in Unreal Engine 5.
    
    However, these are not primary use cases.  The A770 integrates better with the
    Linux stack, has better day-to-day rendering still, though the gap is closing
    now that explicit sync is working.
    
    For now, stick with Intel :)

--- chroot/desktop.sh
+++ chroot/desktop.sh
@@ -5,13 +5,7 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../parameters.sh"
 
 # Video capabilities
-${PACMAN_INSTALL} xorg-server xorg-xeyes nvidia-open nvidia-utils
-systemctl enable nvidia-{hibernate,suspend,resume}
-cat >> /etc/modprobe.d/nvidia.conf <<EOF
-options nvidia NVreg_PreserveVideoMemoryAllocations=1
-options nvidia-drm modeset=1
-options nvidia-drm fbdev=1
-EOF
+${PACMAN_INSTALL} xorg-server xorg-xeyes mesa mesa-utils vulkan-intel vulkan-tools
 
 # Audio capabilities
 ${PACMAN_INSTALL} pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber
--- user/packages/install.sh
+++ user/packages/install.sh
@@ -13,7 +13,7 @@ sudo ${PACMAN_INSTALL} hugin dcraw pinta
 
 # Multimedia
 sudo ${PACMAN_INSTALL} gnome-music celluloid handbrake handbrake-cli mediainfo-gui yt-dlp
-sudo ${PACMAN_INSTALL} gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gst-plugin-pipewire
+sudo ${PACMAN_INSTALL} gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gst-plugin-pipewire gstreamer-vaapi onevpl-intel-gpu libva-utils
 
 # Network
 sudo ${PACMAN_INSTALL} gnome-browser-connector signal-desktop xdg-desktop-portal-gnome
