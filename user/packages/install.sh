c7afc65402f70f9c22d4877fafd842fc8c90b166
George Kramer <12485591+gtkramer@users.noreply.github.com>
2024-09-02 18:50:48 -0700

    Switch to Celluloid for video playback
    The MPV backend is rock solid, and performs better than the GStreamer backend
    with Totem

--- user/packages/install.sh
+++ user/packages/install.sh
@@ -12,7 +12,7 @@ sudo ${PACMAN_INSTALL} clang lldb dotnet-sdk
 sudo ${PACMAN_INSTALL} hugin dcraw pinta
 
 # Multimedia
-sudo ${PACMAN_INSTALL} gnome-music totem handbrake handbrake-cli mediainfo-gui yt-dlp
+sudo ${PACMAN_INSTALL} gnome-music celluloid handbrake handbrake-cli mediainfo-gui yt-dlp
 sudo ${PACMAN_INSTALL} gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gst-plugin-pipewire
 
 # Network
