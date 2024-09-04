2adf744c40f0f91457d4bcbc251d80ab1e690a22
George Kramer <george.kramer@live.com>
2024-09-04 00:00:15 -0700

    Import GPG key for Yubico Authenticator

--- user/devices/yubi.sh
+++ user/devices/yubi.sh
@@ -4,5 +4,6 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../../parameters.sh"
 
+gpg --recv-keys 20EE325B86A81BCBD3E56798F04367096FBA95E8
 ${AURMAN_INSTALL} yubico-authenticator-bin
 sudo systemctl enable --now pcscd.socket
