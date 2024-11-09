441f5a557bbc34416ea9ea98227dc6952ad72112
George Kramer <george.kramer@live.com>
2024-11-08 23:42:07 -0800

    ue5: Update install process
    Build in $HOME/Applications
    
    Do a shallow clone from a branch
    
    Remove extra hacks and workarounds that are no longer needed

--- user/ue5.sh
+++ user/ue5.sh
@@ -4,22 +4,18 @@ set -ex
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../parameters.sh"
 
-BUILD_DIR="${HOME}/Builds/Epic/UnrealEngine"
+BUILD_DIR="${HOME}/Applications/UnrealEngine"
 if [[ ! -e "${BUILD_DIR}/.git" ]]; then
     rm -rf "${BUILD_DIR}"
     mkdir -p "${BUILD_DIR}"
-    git clone git@github.com:EpicGames/UnrealEngine.git -b release "${BUILD_DIR}"
+    git clone --depth 1 --branch release git@github.com:EpicGames/UnrealEngine.git "${BUILD_DIR}"
 else
     pushd "${BUILD_DIR}"
     git reset --hard origin/release
     git clean -dxf
     git pull
-    git clean -dxf
     popd
 fi
-
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
-sudo ln -sf /etc/ssl /usr/lib/ssl
 
 cd "${BUILD_DIR}"
 ./Setup.sh
