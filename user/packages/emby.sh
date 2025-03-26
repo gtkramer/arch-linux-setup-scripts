23b4338deea2e2c292e7292ecaecb3066e231edb
George Kramer <george.kramer@live.com>
2025-03-25 21:23:57 -0700

    Add Emby for streaming media from desktop to TV

---
+++ user/packages/emby.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+. "${SCRIPT_DIR}/../../parameters.sh"
+
+sudo ${PACMAN_INSTALL} emby-server
+sudo systemctl enable --now emby-server
+sudo ufw allow from 192.168.1.0/24 to any port 8096 proto tcp
