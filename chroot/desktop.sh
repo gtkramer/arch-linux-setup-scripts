78679e4977bd0519008f72e9f9156bd0da0d3124
George Kramer <george.kramer@live.com>
2025-10-08 23:09:46 -0700

    Switch back to Intel graphics
    Do this to gear up for bulk video encoding using hardware-accelerated AV1

--- chroot/desktop.sh
+++ chroot/desktop.sh
@@ -5,7 +5,11 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../parameters.sh"
 
 # Video capabilities
-${PACMAN_INSTALL} xorg-server xorg-xeyes nvidia-open nvidia-utils vulkan-tools
+${PACMAN_INSTALL} xorg-server xorg-xeyes mesa mesa-utils vulkan-intel vulkan-tools intel-gpu-tools
+cat >> /etc/modprobe.d/intel-xe.conf <<EOF
+options i915 force_probe=!56a0
+options xe force_probe=56a0
+EOF
 
 # Audio capabilities
 ${PACMAN_INSTALL} pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber
--- user/packages/install.sh
+++ user/packages/install.sh
@@ -16,7 +16,7 @@ sudo ${PACMAN_INSTALL} solaar
 
 # Multimedia
 sudo ${PACMAN_INSTALL} gnome-music celluloid ffmpegthumbnailer handbrake handbrake-cli mediainfo-gui yt-dlp
-sudo ${PACMAN_INSTALL} gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gst-plugin-pipewire
+sudo ${PACMAN_INSTALL} gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gst-plugin-pipewire gstreamer-vaapi vpl-gpu-rt libva-utils
 sudo ${PACMAN_REMOVE_ALL} totem
 
 # Network
