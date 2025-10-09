389dc0fc3ffd8d9e4159a6117cb460a048654df2
George Kramer <george.kramer@live.com>
2025-10-09 00:29:18 -0700

    Rely on vanilla VSCode configurations
    Try not applying font substitutions out of the box to see if this has improved
    over the past few years

--- user/packages/visual-studio-code/install.sh
+++ user/packages/visual-studio-code/install.sh
@@ -5,8 +5,6 @@ SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../../../parameters.sh"
 
 ${AURMAN_INSTALL} visual-studio-code-bin
-sudo cp -f "${SCRIPT_DIR}/98-visual-studio-code.conf" /usr/share/fontconfig/conf.avail
-sudo ln -sf /usr/share/fontconfig/conf.avail/98-visual-studio-code.conf /etc/fonts/conf.d
 
 mkdir -p "${HOME}/.config/Code/User"
 cp -f "${SCRIPT_DIR}/settings.json" "${HOME}/.config/Code/User"
