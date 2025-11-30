3293e797d564d9e582f4cd3cf0382617f518b142
George Kramer <george.kramer@live.com>
2025-11-30 11:40:10 -0800

    Choose tesseract data for emby

--- user/packages/emby.sh
+++ user/packages/emby.sh
@@ -4,6 +4,6 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../../parameters.sh"
 
-sudo ${PACMAN_INSTALL} emby-server
+sudo ${PACMAN_INSTALL} emby-server tesseract-data-eng
 sudo systemctl enable --now emby-server
 sudo ufw allow from 192.168.1.0/24 to any port 8096 proto tcp
