f3cb18bba1d56cce906afcccae0df353af04e1e2
George Kramer <george.kramer@live.com>
2026-03-03 22:45:09 -0800

    Use application profiles for firewall management
    Using an application profile makes it easy to remove the firewall
    changes that were needed for a specific application, if the application
    is ever uninstalled.  It creates a better structure around how a
    firewall is maanged with the base layer and then the application layer
    opening up ports selectively on an as-needed basis.

--- user/packages/emby.sh
+++ user/packages/emby.sh
@@ -6,4 +6,14 @@ readonly SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 
 pacman_install emby-server tesseract-data-eng
 sudo systemctl enable --now emby-server
-sudo ufw allow from 192.168.1.0/24 to any port 8096 proto tcp
+
+# Install UFW application profile for Emby
+sudo mkdir -p /etc/ufw/applications.d
+sudo tee /etc/ufw/applications.d/emby > /dev/null <<'EOF'
+[Emby]
+title=Emby Media Server
+description=Emby media server LAN access
+ports=8096/tcp
+EOF
+sudo ufw app update Emby
+sudo ufw allow from 192.168.1.0/24 to any app Emby
