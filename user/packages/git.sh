1a4c372b88b73b2197cb8f11472424b8683e29e9
George Kramer <george.kramer@live.com>
2026-03-04 17:29:46 -0800

    Skip installing OpenSSH
    Remove an attack vector completely and use HTTPS for working with Git
    repositories

--- user/packages/git.sh
+++ user/packages/git.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 readonly SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 . "${SCRIPT_DIR}/../../common.sh"
 
-pacman_install git openssh "${GIT_EDITOR}"
+pacman_install git "${GIT_EDITOR}"
 git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
 git config --global core.editor "${GIT_EDITOR}"
