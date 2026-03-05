257528e45bfb74e1b709f1d42ea97f6c761da86d
George Kramer <george.kramer@live.com>
2026-03-05 13:45:02 -0800

    Streamline disk handling during initial install
    Assume installs are destructive, that's generally the point of coming to
    these scripts.  That allows simple usage of just `install.sh
    /dev/nvme0n1` to get an install going.  Passing `-p` is what will
    preserve existing data.
    
    Hidden dot files are now always cleaned by default, since this is a
    fresh system install and anything could have changed.

--- boot/install.sh
+++ boot/install.sh
@@ -7,23 +7,18 @@ readonly SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 usage() {
     local script_name
     script_name="$(basename "${0}")"
-    echo "Usage: ${script_name} [-d] [-c] <block device>"
+    echo "Usage: ${script_name} [-p] <block device>"
     echo
-    echo "  -d  Destroy all existing data"
-    echo "  -c  Clean dot files and folders for all users in /home partition"
+    echo "  -p  Preserve data in /home"
     echo "  -h  Show this help message"
 }
 
 # Parse parameters
-destroy_data=false
-clean_dot=false
-while getopts "dch" opt; do
+preserve_home=false
+while getopts "ph" opt; do
     case "${opt}" in
-        d)
-            destroy_data=true
-            ;;
-        c)
-            clean_dot=true
+        p)
+            preserve_home=true
             ;;
         h)
             usage
@@ -35,36 +30,48 @@ while getopts "dch" opt; do
             ;;
     esac
 done
+
 shift $((OPTIND - 1))
 
-if [[ "${#}" -lt 1 ]]; then
+if [[ "${#}" -ne 1 ]]; then
     usage >&2
-    die "Block device is required."
+    die "Exactly one block device is required."
 fi
 
 block_dev="${1}"
 
+# Validate arguments
 if [[ ! -e "${block_dev}" ]]; then
     die "Block device ${block_dev} does not exist."
 fi
 
-if "${destroy_data}"; then
-    warn "You have chosen to destroy all existing data. This operation is irreversible."
-    echo "If you wish to abort this operation, press Ctrl+C within the next 5 seconds."
-    sleep 5s
+if blkid -p -s PTTYPE -o value "${block_dev}" &> /dev/null; then
+    have_partitions=true
+else
+    have_partitions=false
+fi
+
+if "${preserve_home}" && ! "${have_partitions}"; then
+    die "Cannot preserve data in /home on a block device without partitions."
+fi
+
+if ! "${preserve_home}" && "${have_partitions}"; then
+    warn "All existing data in /home will be destroyed."
+    echo "To abort this operation, press Ctrl+C within the next 10 seconds..."
+    sleep 10s
 fi
 
 # Update system clock
 timedatectl set-ntp true
 
 # Create physical partitions
-if "${destroy_data}"; then
+if ! "${preserve_home}"; then
     sgdisk -Z "${block_dev}"
     sgdisk -n 1:1M:+512M -t 1:ef00 "${block_dev}"
     last_usable_sector="$(sgdisk -E "${block_dev}" | grep -P '^\d+$')"
     sgdisk -n 2:0:$(( last_usable_sector - (last_usable_sector + 1) % 2048 )) -t 2:8309 "${block_dev}"
     if ! sgdisk -v "${block_dev}"; then
-        die "Physical partitions failed verification for ${block_dev}"
+        die "Physical partitions failed verification for ${block_dev}."
     fi
 fi
 
@@ -77,7 +84,7 @@ boot_part_dev="${block_dev}${part_separator}1"
 luks_part_dev="${block_dev}${part_separator}2"
 
 # Set up LUKS device
-if "${destroy_data}"; then
+if ! "${preserve_home}"; then
     cryptsetup -y -v luksFormat "${luks_part_dev}"
 fi
 luks_dev=/dev/mapper/${LUKS_NAME}
@@ -92,7 +99,7 @@ fi
 
 vg_name="${VG_NAME}"
 vg_dev="/dev/${vg_name}"
-if "${destroy_data}"; then
+if ! "${preserve_home}"; then
     pvcreate "${luks_dev}"
     vgcreate "${vg_name}" "${luks_dev}"
 
@@ -109,13 +116,13 @@ else
     done
 fi
 if [[ ! -e "${vg_dev}" ]]; then
-    die "${vg_dev} volume device does not exist"
+    die "${vg_dev} volume device does not exist."
 fi
 
 # Create file systems
 mkfs.fat -F32 "${boot_part_dev}"
 mkfs.ext4 -F "${vg_dev}/${LV_ROOT}"
-if "${destroy_data}"; then
+if ! "${preserve_home}"; then
     mkfs.ext4 -F "${vg_dev}/${LV_HOME}"
 fi
 mkswap "${vg_dev}/${LV_SWAP}"
@@ -143,10 +150,8 @@ crypttab_file=/mnt/etc/crypttab.initramfs
 luks_uuid="$(cryptsetup luksUUID "${luks_part_dev}")"
 echo "${LUKS_NAME}       UUID=${luks_uuid}    none    luks" > "${crypttab_file}"
 
-# Post install cleanup of dot files and folders in /home partition for all users
-if "${clean_dot}"; then
-    find /mnt/home -mindepth 2 -maxdepth 2 -name '.*' -exec rm -rf {} +
-    find /mnt/home -mindepth 1 -maxdepth 1 -type d | while read -r home_dir; do
-        rsync --chown="$(stat -c '%U:%G' "${home_dir}")" -a /mnt/etc/skel/ "${home_dir}/"
-    done
-fi
+# Clean dot files and folders in /home partition for all users
+find /mnt/home -mindepth 2 -maxdepth 2 -name '.*' -exec rm -rf {} +
+find /mnt/home -mindepth 1 -maxdepth 1 -type d | while read -r home_dir; do
+    rsync --chown="$(stat -c '%U:%G' "${home_dir}")" -a /mnt/etc/skel/ "${home_dir}/"
+done
