86a3119d5abee234d97ad68afa50f467c065965f
George Kramer <george.kramer@live.com>
2026-03-04 18:49:19 -0800

    Fix execute permissions on scripts

--- user/packages/smart/smart.sh
+++ user/packages/smart/smart.sh
@@ -10,4 +10,3 @@ sudo systemctl enable smartd
 readonly NOTIFY_SCRIPT='/etc/smartmontools/smartd_warning.d/notify.sh'
 sudo mkdir -p "$(dirname "${NOTIFY_SCRIPT}")"
 sudo cp -f "${SCRIPT_DIR}/$(basename "${NOTIFY_SCRIPT}")" "${NOTIFY_SCRIPT}"
-sudo chmod +x "${NOTIFY_SCRIPT}"
