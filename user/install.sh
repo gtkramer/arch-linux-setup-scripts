4f45b4f667bfea635ce9a8d1e7d8302cbf854ee9
George Kramer <george.kramer@live.com>
2026-03-05 18:43:19 -0800

    Skip installing virtualization by default
    This has hardly ever been actually used

--- user/install.sh
+++ user/install.sh
@@ -6,7 +6,7 @@ SCRIPT_NAME="$(basename "${0}")"
 readonly SCRIPT_DIR SCRIPT_NAME
 . "${SCRIPT_DIR}/../common.sh"
 
-configs=(package-manager packages fonts devices settings terminal xdg virtualization)
+configs=(package-manager packages fonts devices settings terminal xdg)
 for config in "${configs[@]}"; do
     config_path="${SCRIPT_DIR}/${config}.sh"
     if [[ ! -e "${config_path}" ]]; then
