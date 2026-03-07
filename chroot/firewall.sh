16ad6df9ea9d6efb1a6dfdbce6dc8d931599bc45
George Kramer <george.kramer@live.com>
2026-03-06 20:14:29 -0800

    Turn on firewall once all rules are set

--- chroot/firewall.sh
+++ chroot/firewall.sh
@@ -10,8 +10,8 @@ pacman_remove iptables
 pacman_install gufw iptables-nft
 systemctl enable ufw
 
-ufw enable
 ufw default deny incoming
 ufw default deny forward
 ufw default allow outgoing
 ufw logging on
+ufw enable
